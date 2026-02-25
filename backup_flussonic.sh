f21868721f071698d2e6c1f0afaa8007947f0ea9
NOC-OLETV <github@oletv.net.br>
2026-02-24 21:17:13 -0300

    Create backup_flussonic.sh

---
+++ backup_flussonic.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#Instalando pacotes
+apt install -y postfix mailutils mutt 
+
+#Criando arquivo para o scrip
+cat <<EOF > /usr/local/src/backup_flussonic.sh
+#!/bin/bash
+
+ORIGEM="/etc/flussonic/flussonic.conf"
+DESTINO="/etc/flussonic/backup"
+
+DATA=$(date +%d%m%Y)
+
+ARQUIVO_BACKUP="$DESTINO/backup_flussonic_192.168.234.10_$DATA.conf"
+
+#COPIANDO LOCALMENTE
+cp "$ORIGEM" "$ARQUIVO_BACKUP" 2>>/etc/flussonic/logs_backup
+
+#ENVIADO E-MAIL
+mutt -s "[Flussonic Backup] ENC-192.168.234.10 $DATA" -a "$ARQUIVO_BACKUP" -- noc@oletv.net.br < /dev/null 2>>/etc/flussonic/logs_backup
+EOF
+chmod +x /usr/local/src/backup_flussonic.sh 
+
+#Criando diretorio para backup
+mkdir /etc/flussonic/backup
+
+#Criando arquivo para salvar os logs do backup
+cd /etc/flussonic/
+touch logs_backup
+
+#Criando sasl_passwd
+touch /etc/postfix/sasl_passwd
+postmap /etc/postfix/sasl_passwd
+chmod 600 /etc/postfix/sasl_passwd*
+systemctl restart postfix
