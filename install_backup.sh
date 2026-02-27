dd8e0efe60784a233fedee3e6b49d5bb35104543
NOC-OLETV <github@oletv.net.br>
2026-02-27 17:24:33 -0300

    Update install_backup.sh

--- install_backup.sh
+++ install_backup.sh
@@ -3,9 +3,9 @@ apt install -y postfix mailutils mutt
 
 #ALTERANDO SERVIDOR DE E-MAIL
 sed -i 's/relayhost = /relayhost = [corpmail.ole.net.br]:25/' /etc/postfix/main.cf
-»smtp_sasl_auth_enable = yes /etc/postfix/main.cf
-»smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd /etc/postfix/main.cf
-»smtp_sasl_security_options = noanonymous /etc/postfix/main.cf
+echo smtp_sasl_auth_enable = yes >> /etc/postfix/main.cf
+echo smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd >> /etc/postfix/main.cf
+echo smtp_sasl_security_options = noanonymous >> /etc/postfix/main.cf
 
 #CONFIGURANDO CREDENCIAS
 echo [corpmail.ole.net.br]:25 monitoramento@oletv.net.br:MOnitor@@2026 > /etc/postfix/sasl_passwd
